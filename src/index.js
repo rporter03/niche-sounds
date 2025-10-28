bf129349263243acf82fe7c0e224b2c58136977a
rporter03 <porterclayton2003@gmail.com>
2025-10-27 22:44:45 -0400

    Fix index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './../public/index.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Layout from './Layout';
+import Home from './index.js';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import Contact from './pages/Contact';
+import DiscoverArtists from './pages/Discover-Artists';
+import Forums from './pages/Forums';
+
+export default function App() {
+  return (
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="about" element={<About />}/>
+          <Route path="blog" element={<Blog />}/>
+          <Route path="contact" element={<Contact />}/>
+          <Route path="discover-artists" element={<DiscoverArtists />}/>
+          <Route path="forums" element={<Forums />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>hello world</h1>
+    <App />
   </React.StrictMode>
 );
