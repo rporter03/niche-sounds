ed98bf1adc6a53dc594981b5375234c597e8bcda
rporter03 <porterclayton2003@gmail.com>
2025-11-02 23:21:57 -0500

    Project Part 9

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,16 +1,29 @@
 import {Link} from "react-router-dom";
+import {useState} from 'react';
 import "./../css/Navigation.css";
 
 const Navigation = () => {
+
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [downArrow, setDownArrow] = useState('true');
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+        setDownArrow(!downArrow);
+    };
+
     return (
         <nav id="main-nav">
-            <ul class="columns">
+            <a onClick={toggleMenu} id="toggle-nav" href="#">
+                {downArrow ? (<p>&darr;</p>) : (<p>&uarr;</p>)}
+            </a>
+            <ul className={menuOpen ? "columns" : "hide-small"}>
                 <li><Link to="/">Home</Link></li>
-                <li><Link to="/">About</Link></li>
-                <li><Link to="/">Blog</Link></li>
-                <li><Link to="/">Discover Aritsts</Link></li>
-                <li><Link to="/">Forums</Link></li>
-                <li><Link to="/">Contact</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="/blog">Blog</Link></li>
+                <li><Link to="/discover-artists">Discover Aritsts</Link></li>
+                <li><Link to="/forums">Forums</Link></li>
+                <li><Link to="/contact">Contact</Link></li>
             </ul>
         </nav>
     );
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,39 @@
 import './../css/Contact.css';
+import {useState} from 'react';
 
 const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending...")
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "4256cc32-1920-4952-a04f-b36af3f147f2");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const res = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json"
+      },
+      body: json
+    }).then((res) => res.json());
+
+    if (res.success) {
+        console.log("Success", res);
+        setResult("Email Successfully Sent")
+        event.target.reset(); // reset form fields
+    } else {
+        console.log("Error sending email", res);
+        setResult("Error Sending Email");
+    }
+    };
+
     return (
         <main id="contact">
             <h2>Contact</h2>
@@ -17,7 +50,7 @@ const Contact = () => {
                     <img src="images/music-note.svg" alt="Music Note Picture"/>
                 </div>
                 <section className="one">
-                    <form method="POST" id="contact-form">
+                    <form onSubmit={onSubmit} id="contact-form">
 
                         <input type="hidden" name="access_key" value="4256cc32-1920-4952-a04f-b36af3f147f2"/>
 
@@ -33,11 +66,12 @@ const Contact = () => {
                         <label for="message">Message:</label>
                         <textarea name="message" required></textarea>
                             
-                        <input type="checkbox" name="botcheck" className="hidden" style="display: none;"/>
+                        <input type="checkbox" name="botcheck" className="hidden"/>
 
                         <p><button type="submit">Submit Form</button></p>
 
                         <div id="contact-result"></div>
+                        <span>{result}</span>
                     </form>
                 </section>
             </section>
